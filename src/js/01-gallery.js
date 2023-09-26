792bcdfc94cccbac99cf785d4a8a55cb6564b822
Lena-Yasynetska <123744975+Lena-Yasynetska@users.noreply.github.com>
2023-09-26 21:49:43 +0200

    8-1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+
+import { galleryItems } from './gallery-items';
+
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(item =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image"
+    src="${item.preview}"
+    data-source="${item.original}"
+    alt="${item.description}" />
+    </a>
+    </li>`)
+    .join('');
+
+galleryList.innerHTML = galleryMarkup;
+galleryList.addEventListener("click", e => {
+    e.preventDefault();
+    if (e.target.tagName === "IMG") {
+        const largeImageSrc = e.target.getAttribute("data-source");
+        const lightbox = basicLightbox.create(`<img src="${largeImageSrc}" alt="${e.target.alt}" />`);
+        lightbox.show();
+    }
+});
+
+console.log(galleryItems);
---
+++ src/js/03-feedback.js
@@ -0,0 +1,32 @@
+import throttle from "lodash.trottle";
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+form.addEventListener('input', throttle(saveFormData, 500));
+form.addEventListener('submit', onSubmit);
+
+function saveFormData(){
+    const formData = {
+        email: emailInput.value,
+        message: messageInput.value,
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}
+function onSubmit(event){
+    event.preventDefault();
+    emailInput.value = '';
+    messageInput.value = '';
+    localStorage.removeItem("feedack-form-state");
+}
+
+function dataLokalStorage(){
+    const storedData = localStorage.getItem("feedback-form-state");
+    if (storedData !== null) {
+        const formData = JSON.parse(storedData);
+        emailInput.value = formData.email;
+        messageInput.value = formData.message;
+    }
+    console.log(storedData);
+}
